9fad91e3c27eb8be5171302aca0ef9f11b85bc8a
SAFEluren <safeluren@Outlook.com>
2024-02-18 14:20:40 +0800

    1

--- nonebot_plugin_bf1marneserverlist/plugins/__init__.py
+++ nonebot_plugin_bf1marneserverlist/plugins/__init__.py
@@ -7,12 +7,12 @@ from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
 from nonebot.permission import SUPERUSER
 
-from .utils import *
+import httpx
 from .config import Config
 
 from pathlib import Path
 import json
-
+import httpx
 
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-bf1marneserverlist",
@@ -35,8 +35,10 @@ __plugin_meta__ = PluginMetadata(
 
 plugin_config = get_plugin_config(Config)
 
+marne_url = plugin_config.marne_url
+    
 CURRENT_FOLDER = Path(plugin_config.marne_data_dir).resolve()
-CURRENT_FOLDER.mkdir(exist_ok=True)
+CURRENT_FOLDER.mkdir(parents=True,exist_ok=True)
 
 async def is_enable() -> bool:
     return plugin_config.marne_plugin_enabled
@@ -45,16 +47,36 @@ MARNE_MAIN = on_command(f'marne', block=True, priority=1)
 MARNE_BIND = on_command(f'marne bind', block=True, priority=1,permission=GROUP_OWNER | GROUP_ADMIN | SUPERUSER)
 MARNE_MODS = on_command(f'marne modlist', block=True, priority=1)
 
+async def request_marneAPI(serverID):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{marne_url}api/srvlst/{serverID}")
+
+            content = response.text
+            if content is not None:
+                return content
+            else:
+                print("Response content is None")
+
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e}")
 
 @MARNE_MAIN.handle()
 async def marne_info(event:GroupMessageEvent, state:T_State):
     session = event.group_id
 
-    with open(CURRENT_FOLDER/f'{session}.json','r', encoding='utf-8') as f:
-        group = json.load(f)
+    try:
+        with open(CURRENT_FOLDER/f'{session}.json','r', encoding='utf-8') as f:
+            group = json.load(f)
+    except FileNotFoundError:
+        await MARNE_MAIN.send('请先绑定服务器ID.')
+        return
     serverID = group['id']
-    result = request_marneAPI(serverID)
-    result = json.loads(result)
+    results = await request_marneAPI(serverID)
+    if results is not None:
+        result = json.loads(results)
+    else:
+        print("Response content is None")
 
     if result:
         server_ID = result['id']
@@ -92,8 +114,7 @@ async def marne_mods(event:GroupMessageEvent, state:T_State):
     session = event.group_id
 
 @MARNE_BIND.handle()
-async def marne_bind(event:GroupMessageEvent, state:T_State):
-
+async def marne_bind(event: GroupMessageEvent, state: T_State):
     message = _command_arg(state) or event.get_message()
     session = event.group_id
     args = message.extract_plain_text().strip().split(' ')
@@ -102,24 +123,27 @@ async def marne_bind(event:GroupMessageEvent, state:T_State):
         serverID = int(args[0])
     except ValueError:
         await MARNE_BIND.send('格式错误，仅允许纯数字')
+        return  # 在这里返回，避免继续执行代码
+
     print(serverID)
-    result = request_marneAPI(serverID)
-    result = json.loads(result)
-    if result:
+    result = await request_marneAPI(serverID)  # 等待异步函数完成
+    results = await request_marneAPI(serverID)
+    if results is not None:
+        result = json.loads(results)
         serverName = result['name']
-        with open(CURRENT_FOLDER/f'{session}.json','w', encoding='utf-8') as f:
-            json.dump(result, f, ensure_ascii=False, indent=4)
+        try:
+            with open(CURRENT_FOLDER / f'{session}.json', 'w', encoding='utf-8') as f:
+                    json.dump(result, f, ensure_ascii=False, indent=4)
+        except FileNotFoundError:
+            CURRENT_FOLDER.mkdir(parents=True,exist_ok=True)
+            return
 
         msg = Message([MessageSegment.text(f"绑定成功！")])
         msg.append(f"\n绑定服务器ID :{serverID}")
         msg.append(f"\n服务器名字:{serverName}")
-        
+
         await MARNE_BIND.send(msg)
         # await MARNE_BIND.send(f'已绑定服务器ID:{serverID}')
     else:
         print(result)
         await MARNE_MAIN.send('无法获取到服务器数据，请检查马恩服务器id是否正确，或服务器当前未开启。')
-        return
-
-
-
--- nonebot_plugin_bf1marneserverlist/plugins/config.py
+++ nonebot_plugin_bf1marneserverlist/plugins/config.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 class Config(BaseModel):
     marne_url: str
     marne_plugin_enabled: bool = True
-    marne_data_dir: str = './marne_data/'
+    marne_data_dir: str = './data/marne_data/'
     class Config:
         extra = "ignore"
